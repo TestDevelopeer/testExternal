a768180d78b8517fcc9935af215cb6a9e2483229
Igor Vereschinsky <testdeveloper7967@gmail.com>
2023-04-24 11:55:03 +0300

    Update ros.js

--- ros.js
+++ ros.js
@@ -29,4 +29,59 @@ $(document).on('focusout', '#form2-servicename', function(){
   $('#select2-form2-okeiid-container').click();
   $('.select2-search--dropdown .select2-search__field').val('642');
   $('#select2-form2-okeiid-results li:first').click();
+  
+  function setSelect2Value(selectId, inputClass, inputValue) {
+  let promise = new Promise((resolve, reject) => {
+    $(selectId).select2('open')
+    input = document.querySelector(inputClass);
+    input.focus();
+    input.value = inputValue;
+    input.dispatchEvent(new KeyboardEvent('keydown', {
+      bubbles: true
+    }));
+    input.dispatchEvent(new KeyboardEvent('keypress', {
+      bubbles: true
+    }));
+    input.dispatchEvent(new KeyboardEvent('keyup', {
+      bubbles: true
+    }));
+    input.dispatchEvent(new Event('input', {
+      bubbles: true
+    }));
+    input.dispatchEvent(new Event('change', {
+      bubbles: true
+    }));
+    input.focus();
+    setTimeout(function() {
+      const ke = new KeyboardEvent('keydown', {
+        bubbles: true,
+        cancelable: true,
+        keyCode: 13
+      });
+      input.dispatchEvent(ke);
+
+      resolve("result");
+    }, 500);
+  });
+
+  return promise;
+}
+
+setSelect2Value('#form2-okeiid', '.select2-search--dropdown .select2-search__field', '642').then(
+  result => {
+    setSelect2Value('#form2-tnvedid', '.select2-search--dropdown .select2-search__field', '8423 90 000').then(
+      result => {
+        setSelect2Value('#form2-industryid', '.select2-search--inline .select2-search__field', 'Машиностроение').then(
+            result => {
+              setSelect2Value('#form2-industryid', '.select2-search--inline .select2-search__field', 'Сельско').then(
+                  result => {
+                    $('#form2-industrialplatformid option[value=17718127]').attr('selected','selected');
+                  }
+                )
+            }
+          )
+      }
+    )
+  }
+);
 });
