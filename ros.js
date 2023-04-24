1a70f9ec2c26a441e29d581e56570c9d6900b681
Igor Vereschinsky <testdeveloper7967@gmail.com>
2023-04-24 10:12:03 +0300

    Add files via upload

---
+++ ros.js
@@ -0,0 +1,12 @@
+let scales = [
+  {
+    name: 'Весы автомобильные в приямке АСП-40-8-1-П Цифровые датчики Kelli (Китай), Zemic (США)',
+  },
+  {
+    name: 'Весы автомобильные в приямке АСП-40-8-1-П Цифровые датчики HBM (Германия)'
+  }
+];
+
+$(document).on('input change', '#form2-servicename', function(){
+  console.log($(this).val());
+});
